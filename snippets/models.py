9c0a776eb5272e841312ba5e3c2b687fc4ef8017
Ruslan9111 <ruslan_ru911@mail.ru>
2022-12-14 15:49:04 +0500

    second commit

--- snippets/models.py
+++ snippets/models.py
@@ -9,6 +9,9 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
+    """
+    Модель будет использоваться для хранения блоков кода(Сниппетов).
+    """
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
