e116c2d98539508064f3a99b8871c0a3a1623fe0
Ruslan9111 <ruslan_ru911@mail.ru>
2022-12-19 11:46:10 +0500

    add movies

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework import routers
+#
+# from snippets.views import snippet_list, snippet_detail
+#
+# router = routers.DefaultRouter()
+#
+# router.register(r'snippets', snippet_list, basename='list_of_snippets')
+# router.register(r'snippets/<int:pk>', snippet_detail, basename='detail_snippet')
 
-from snippets.views import snippet_list, snippet_detail
-
-router = routers.DefaultRouter()
-router.register(r'snippets', snippet_list)
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
 
 ]
+
+# urlpatterns += router.urls
